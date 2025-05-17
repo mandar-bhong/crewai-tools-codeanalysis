a407748ed43aa1dfc5f90fc4410b829acff90c63
Mandar <mandar-bhong@users.noreply.github.com>
2025-05-18 01:53:34 +0530

    Draft commit

---
+++ src/codeanalysis/tool.py
@@ -0,0 +1,10 @@
+from crewai.tools import BaseTool
+
+
+class Codeanalysis(BaseTool):
+    name: str = "Name of my tool"
+    description: str = "You are expert in software code analysis and extracting class, function and module. Also able to form the relationship between it"
+
+    def _run(self, argument: str) -> str:
+        # Your tool's logic here
+        return "Tool's result"
