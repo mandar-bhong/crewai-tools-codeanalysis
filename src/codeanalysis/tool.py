6e77fb640eb03b2cd1e7d91d79ee0215d593274f
Mandar <mandar-bhong@users.noreply.github.com>
2025-05-18 02:09:07 +0530

    Update tool.py

--- src/codeanalysis/tool.py
+++ src/codeanalysis/tool.py
@@ -2,7 +2,7 @@ from crewai.tools import BaseTool
 
 
 class Codeanalysis(BaseTool):
-    name: str = "Name of my tool"
+    name: str = "Code Analysis"
     description: str = "You are expert in software code analysis and extracting class, function and module. Also able to form the relationship between it"
 
     def _run(self, argument: str) -> str:
